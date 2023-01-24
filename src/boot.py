a50d3e86e016985a8eb145e97af06c7d0e18ac4b
dr. josh butler <josh.butler929@gmail.com>
2023-01-23 23:58:51 -0500

    update boot.py

--- src/boot.py
+++ src/boot.py
@@ -1,13 +1,13 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
+import esp
 import machine
 
-# disable UART 1 at boot
-uart1 = machine.UART(1, 115200)
-uart1.deinit()
+
+esp.osdebug(0)
+#esp.osdebug(None)
+
+#import webrepl
+#webrepl.start()
 
 # Set frequency to 240MHz
 machine.freq(240000000)
